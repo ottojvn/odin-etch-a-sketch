d9e3b113010c9ec0f00adc628768ce08c4bf8a10
Joao Victor Otto <ottojvn@gmail.com>
2022-08-25 13:19:58 -0300

    Set default behavior for the grid

--- script.js
+++ script.js
@@ -1,14 +1,43 @@
-const sliderInput = this.document.querySelector("#slider > input");
-let gridSize = sliderInput.value;
-
 function updateRangeText(gridSize) {
   const rangeValue = document.querySelector("#rangeValue");
   rangeValue.textContent = `${gridSize}x${gridSize}`;
 }
 
-function updateGrid() {
-  let gridSize = this.value;
-  updateRangeText(gridSize);
+function populateGrid(gridSize) {
+  board.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+
+  clearChildren(board);
+  for (let i = 0; i < gridSize * gridSize; ++i) {
+    let square = document.createElement("div");
+
+    square.onclick = function () {
+      this.style.backgroundColor = color;
+    };
+
+    board.insertAdjacentElement("beforeend", square);
+  }
+
+  return;
 }
 
+function clearChildren(element) {
+  while (element.firstChild) {
+    element.lastChild.remove();
+  }
+}
+
+function updateGrid() {
+  let gridSize = sliderInput.value;
+
+  updateRangeText(gridSize);
+  populateGrid(gridSize);
+}
+
+const board = document.querySelector("#board");
+const sliderInput = this.document.querySelector("#slider > input");
+let color = "black";
+
 sliderInput.addEventListener("input", updateGrid);
+
+updateGrid();
